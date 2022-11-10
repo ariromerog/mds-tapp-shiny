ede362c53985aac1b6dde7d1103a13bb4ce368cd
Ari Romero <ari.romero@gmail.com>
2022-11-09 22:23:42 -0300

    dynamic sub area

--- app/app.py
+++ app/app.py
@@ -11,67 +11,76 @@ opts_rankings = rankings_df["Ranking"].unique().tolist()
 opts_years = rankings_df["Año"].unique().tolist()
 opts_instituciones = rankings_df["Institucion"].unique().tolist()
 opts_area = rankings_df["Area"].unique().tolist()
-opts_subarea = rankings_df["Subarea"].unique().tolist()
+opts_subarea = ()
 opts_indicador = rankings_df["Indicador"].unique().tolist()
 
 app_ui = ui.page_fluid(
     ui.div(
-        children=[
-            ui.h1("Tarea 1 - Taller de Aplicaciones 2"),
-            ui.p("Integrantes: Ari Romero Garrido - Karen Romero Garrido"),
+    children=[
+        ui.h1("Tarea 1 - Taller de Aplicaciones 2"),
+        ui.p("Integrantes: Ari Romero Garrido - Karen Romero Garrido"),
+    ]),
+    ui.row(
+        ui.column(2, children=[
+            ui.input_selectize(
+                "rankings",
+                "Rankings",
+                opts_rankings,
+                multiple=True
+            ),
         ]),
+        ui.column(2, children=[
+            ui.input_selectize(
+                "instituciones",
+                "Instituciones",
+                opts_instituciones,
+                multiple=True
+            ),
+        ]),
+        ui.column(2, children=[
+            ui.input_selectize(
+                "area",
+                "Área",
+                opts_area,
+                multiple=True
+            ),
+        ]),
+        ui.column(2, children=[
+            ui.output_ui("select_subarea"),
+        ]),
+        ui.column(2, children=[
+            ui.input_selectize(
+                "indicador",
+                "Indicador",
+                opts_indicador,
+                multiple=True
+            ),
+        ]),
+    ),
     ui.hr(),
-    ui.h2("Exploración de Datos"), ui.navset_tab(
-        ui.nav("Exploración Datos", ui.row(
-            ui.column(2, children=[
-                ui.input_selectize(
-                    "rankings",
-                    "Rankings",
-                    opts_rankings,
-                    multiple=True
-                ),
-            ]),
-            ui.column(2, children=[
-                ui.input_selectize(
-                    "instituciones",
-                    "Instituciones",
-                    opts_instituciones,
-                    multiple=True
-                ),
-            ]),
-            ui.column(2, children=[
-                ui.input_selectize(
-                    "area",
-                    "Área",
-                    opts_area,
-                    multiple=True
-                ),
-            ]),
-            ui.column(2, children=[
-                ui.input_selectize(
-                    "subarea",
-                    "Sub Área",
-                    opts_subarea,
-                    multiple=True
-                ),
-            ]),
-            ui.column(2, children=[
-                ui.input_selectize(
-                    "indicador",
-                    "Indicador",
-                    opts_indicador,
-                    multiple=True
-                ),
-            ]),
-        ), ui.output_table("df")),
-        ui.nav("Ranking", "Contenido 1"),
-        ui.nav("Visualización Por Area", "Contenido 1"),
-        ui.nav("Visualización Por Sub Area", "Contenido 1"),
+    ui.navset_tab(
+        ui.nav("Explorar", ui.output_table("df")),
+        ui.nav("Ranking por Area / Sub Area", "Contenido 1"),
+        ui.nav("Ranking por Indicador", "Contenido 1"),
     )
 )
 
 
 def server(input, output, session):
+
+    @output
+    @render.ui()
+    def select_subarea():
+        indx_area = rankings_df["Area"].isin(input.area())
+        sub_df = rankings_df[indx_area]
+        opts_subarea = sub_df["Subarea"].unique().tolist()
+        return ui.input_selectize(
+                "subarea",
+                "Sub Área",
+                opts_subarea,
+                multiple=True
+            )
+
     @output
     @render.table
     def df():
